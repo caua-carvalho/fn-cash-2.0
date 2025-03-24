e15c07749de7336ef2c4aa41366380adbe785532
Mindplus4 <mindplus2024@outlook.com>
2025-03-24 19:26:00 -0300

    Update funcoes.php

--- funcoes.php
+++ funcoes.php
@@ -1,9 +1,9 @@
 <?php
-
 require "conexao.php";
+require "validacao_login.php";
 function cadastro_categoria($nome, $tipo, $categoria_pai, $descricao, $status){
     global $conn;
     global $id_usuario;
-
+    
 }
 ?>
