d5aeefbe557bb873c24a5d3711af9cc57a8782bf
caua-carvalho <caua.carma033@gmail.com>
2025-03-24 17:11:59 -0300

    Update validacao_login.php

--- validacao_login.php
+++ validacao_login.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['id'])){
-    header('Location: ../index.php');
+if(isset($_SESSION['id'])){
+    header('Location: index.php');
     exit();
+}else{
+    header('Location: pages/transacoes.php');
+    exit;
 }
 ?>
