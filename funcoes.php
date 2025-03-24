a26005e20aa8db54209451567201fff18bc248c9
caua-carvalho <caua.carma033@gmail.com>
2025-03-24 13:23:37 -0300

    Update funcoes.php

--- funcoes.php
+++ funcoes.php
@@ -5,8 +5,5 @@ function cadastro_categoria($nome, $tipo, $categoria_pai, $descricao, $status){
     global $conn;
     global $id_usuario;
 
-    // ANTES DE FAZER ESSA FUNCAO ARRUME O LOGIN DO USUARIO SEU MONGOLOIDE
-    // BOA NOITE
-    
 }
 ?>
