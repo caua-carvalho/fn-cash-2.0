b6c9de036612da19e46853b7c7529e2ca0fddd1c
caua-carvalho <caua.carma033@gmail.com>
2025-03-25 19:54:46 -0300

    funcao de cadastro categoria

--- pages/funcoes.php
+++ pages/funcoes.php
@@ -3,6 +3,28 @@
 include_once 'header.php';
 require_once "funcoes.php";
 
+
+function cadastrar_categoia($nome, $tipo, $categoria_pai, $descricao, $status, $id_usuario){
+    $stmt = $conn->prepare("SELECT nome FROM CATEGORIA WHERE nome = ?");
+    $stmt->bind_param("ssisssi", $nome);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if($resultado->num_rows > 0){
+        return "categoria ja cadastrada";
+    } else {
+        $stmt = $conn->prepare("INSERT INTO CATEGORIA (Nome, Tipo, Descricao, Ativa, ID_CategoriaPai, ID_Usuario) VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("sss", $nome, $tipo, $descricao, $status, $categoria_pai, $id_usuario);
+        
+
+        if($stmt->execute()) {
+            return "SUCESSO";
+        } else {
+            return "ERRO";
+        }
+    }
+}
+
 // CADASTRO DE CATEGORIA
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Recuperar os dados do formulário
